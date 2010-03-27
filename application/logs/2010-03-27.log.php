562192349fbd876bc8b8858728158a63853a6726
Horia Dragomir <horia@hdragomir.com>
2010-03-27 13:41:31 +0200

    changed to localhost

--- application/logs/2010-03-27.log.php
+++ application/logs/2010-03-27.log.php
@@ -5,3 +5,23 @@
 2010-03-27 00:21:23 +02:00 --- error: Uncaught PHP Error: mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Access denied for user 'projectslounge'@'88.158.60.35' (using password: YES) in file E:/xampp/htdocs/ProjectsLounge/system/libraries/drivers/Database/Mysql.php on line 61
 2010-03-27 00:21:28 +02:00 --- error: Uncaught PHP Error: mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Access denied for user 'projectslounge'@'88.158.60.35' (using password: YES) in file E:/xampp/htdocs/ProjectsLounge/system/libraries/drivers/Database/Mysql.php on line 61
 2010-03-27 11:11:49 +02:00 --- error: Uncaught PHP Error: mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Access denied for user 'projectslounge'@'88.158.60.35' (using password: YES) in file E:/xampp/htdocs/ProjectsLounge/system/libraries/drivers/Database/Mysql.php on line 61
+2010-03-27 13:36:11 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2010-03-27 13:36:11 +02:00 --- debug: Session Cookie Driver Initialized
+2010-03-27 13:36:11 +02:00 --- debug: Session Library initialized
+2010-03-27 13:36:11 +02:00 --- debug: Auth Library loaded
+2010-03-27 13:36:11 +02:00 --- debug: MySQLi Database Driver Initialized
+2010-03-27 13:36:11 +02:00 --- debug: Database Library initialized
+2010-03-27 13:36:11 +02:00 --- error: Uncaught Kohana_Database_Exception: There was an SQL error: Table 'projectslounge.users' doesn't exist - SHOW COLUMNS FROM `users` in file E:/xampp/htdocs/ProjectsLounge/system/libraries/drivers/Database/Mysqli.php on line 142
+2010-03-27 13:36:31 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2010-03-27 13:36:31 +02:00 --- debug: Session Cookie Driver Initialized
+2010-03-27 13:36:31 +02:00 --- debug: Session Library initialized
+2010-03-27 13:36:31 +02:00 --- debug: Auth Library loaded
+2010-03-27 13:36:31 +02:00 --- debug: MySQL Database Driver Initialized
+2010-03-27 13:36:31 +02:00 --- debug: Database Library initialized
+2010-03-27 13:36:31 +02:00 --- error: Uncaught Kohana_Database_Exception: There was an SQL error: Table 'projectslounge.users' doesn't exist - SHOW COLUMNS FROM `users` in file E:/xampp/htdocs/ProjectsLounge/system/libraries/drivers/Database/Mysql.php on line 371
+2010-03-27 13:39:54 +02:00 --- debug: Global GET, POST and COOKIE data sanitized
+2010-03-27 13:39:54 +02:00 --- debug: Session Cookie Driver Initialized
+2010-03-27 13:39:54 +02:00 --- debug: Session Library initialized
+2010-03-27 13:39:54 +02:00 --- debug: Auth Library loaded
+2010-03-27 13:39:54 +02:00 --- debug: MySQL Database Driver Initialized
+2010-03-27 13:39:54 +02:00 --- debug: Database Library initialized
