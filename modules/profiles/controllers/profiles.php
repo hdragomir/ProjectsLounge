35e651e6058778f6514fa931be3b9005782be65d
cristiansitov <sitov.cristian@gmail.com>
2010-03-28 16:21:51 +0300

    added auth module and login page

--- modules/profiles/controllers/profiles.php
+++ modules/profiles/controllers/profiles.php
@@ -27,7 +27,34 @@ class Profiles_Controller extends Template_Controller{
     }
     
     public function login(){
-        
+
+	//Check if already logged in
+	if (Auth::instance()->logged_in('login')) {
+		url::redirect('index');
+	}
+ 
+	//Initialize template and form fields
+	$view = View::factory( 'profiles/login' );
+	$view->username = '';
+	$view->password = '';
+ 
+	//Attempt login if form was submitted
+	if ($post = $this->input->post()) {
+		if (ORM::factory('user')->login($post)) {
+			url::redirect($this->session->get('requested_url'));
+		} else {
+			$view->username = $post['username']; //Redisplay username (but not password) when form is redisplayed.
+			$view->message = in_array('required', $post->errors()) ? 'Username and password are required.' : 'Invalid username and/or password.';
+		}
+	}
+ 
+	//Display login form
+	//$view->render(TRUE);
+       
+        $this->template->content = $view;
+    }
+
+    public function register(){
         
     }
 }
